6d1cf6bf423fb5b715b621edf7df2747d208beaa
Billy <billyurrutia@gmail.com>
2020-07-27 21:29:42 -0600

    added a logout need log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,4 +10,10 @@ class SessionsController < ApplicationController
 
   def welcome
   end
+  
+  def logout
+    reset_session
+    flash[:message] = "You are logged out!"
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
    get 'login', to: 'sessions#new'
    post 'login', to: 'sessions#create'
    root to: 'sessions#welcome'
+   get 'logout', to: 'sessions#logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
