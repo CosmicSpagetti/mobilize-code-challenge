350c0286c3d16c2b5d39ece2fd78c8d040444183
Billy <billyurrutia@gmail.com>
2020-07-27 20:19:43 -0600

    Create welcome page and sign up page

--- spec/features/user_sign_in_spec.rb
+++ spec/features/user_sign_in_spec.rb
@@ -4,7 +4,7 @@ describe "user can create account" do
   describe "They fill in name password and phone number" do
       it "Creates account" do
         visit root_path
-        click_on "Create Account"
+        click_on "Sign Up"
         expect(current_path).to eq(new_user_path)
 
         fill_in "user[name]", with: "Billy!!"
