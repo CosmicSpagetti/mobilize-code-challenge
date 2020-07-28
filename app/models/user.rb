b7ff671994c983f6f53aca22da7e7a887b4e6e25
Billy <billyurrutia@gmail.com>
2020-07-27 23:26:07 -0600

    fix dumb field stuff

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   validates_presence_of :name,
                         :password_digest
 
-  validates :phone_number, format: { with: /\A\d{3}-\d{3}-\d{4}\z/, message: 'Put a real number!' }, presence: true
+  validates :phone_number, format: { with: /\A\d{3}-\d{3}-\d{4}\z/, message: 'wrong. Put a real number!' }, presence: true
 
   def format_and_check_number
     self.phone_number = ActionController::Base.helpers.number_to_phone(self.phone_number)
